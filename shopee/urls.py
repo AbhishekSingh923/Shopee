27d06c6e9692dfeb03ecd69864662c64adfb45c3
AbhishekSingh923 <876529shiv@gmail.com>
2025-01-21 12:05:08 +0530

    Initial Commit

---
+++ shopee/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('apps.accounts.urls')),
+    path('orders/', include('apps.orders.urls')),
+    path('products/', include('apps.products.urls')),
+    path('payments/', include('apps.payments.urls')),
+    path('shipping/', include('apps.shipping.urls')),
+]
+
